3d9de9692d22fdfe73fc1fd2e5dd11e1f639d1f7
dchang-koverse <126500066+dchang-koverse@users.noreply.github.com>
2023-05-22 13:19:40 -0700

    [CRUC-001][CI] Add Lint PR Title GitHub Action (#2)
    * Add readme
    
    * Update script path
    
    * Print cwd
    
    * Update bash
    
    * Use yarn install action
    
    * get pr number
    
    * Fix async issue
    
    * Update repo token
    
    * Remove token
    
    * run octokit rest shortcut
    
    * type module
    
    * Use core to get token and pr number
    
    * adjust fetch
    
    * Use request object
    
    * print response
    
    * Add actions/core dependency
    
    * without auth
    
    * with auth
    
    * get emojis
    
    * use rest
    
    * Fetch root
    
    * try catch
    
    * print github
    
    * print context
    
    * Add error handling
    
    * Clean up

--- .github/scripts/lint-pr-title.js
+++ .github/scripts/lint-pr-title.js
@@ -1,11 +1,44 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import { Octokit } from "@octokit/rest";
+import core from '@actions/core';
+import github from '@actions/github'
 
 const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
 
-const octokit = github.getOctokit(GITHUB_TOKEN);
+const octokit = new Octokit({
+  auth: GITHUB_TOKEN,
+})
 
-const { context = {} } = github;
-const { pull_request } = context.payload;
+console.log('github.context.payload.pull_request', github.context.payload.pull_request)
 
-console.log(pull_request.pull_request.title)
+try {
+  const response = await octokit.request("GET /repos/{owner}/{repo}/pulls/{pull_number}", {
+    owner: 'dchang-koverse',
+    repo: 'lint-pr',
+    pull_number: github.context.payload.pull_request.number,
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  });
+
+  const title = response.data.title;
+
+  console.log('title', title)
+
+  // Make sure title is in the format of "[CRUC-1234][UI] My title"
+  const regex = /\[CRUC-\d+\]\[.+\] .+/;
+  if (!regex.test(title)) {
+    // if the PR exists, but the title is not in the correct format, fail the check
+    core.setFailed('PR title is not in the correct format. Please use "[CRUC-1234][UI] My description" format.')
+    // const commentResponse = await octokit.request("POST /repos/{owner}/{repo}/issues/{issue_number}/comments", {
+    //   owner: 'dchang-koverse',
+    //   repo: 'lint-pr',
+    //   issue_number: github.context.payload.pull_request.number,
+    //   body: 'PR title is not in the correct format. Please use "[CRUC-1234][UI] My description" format.'
+    // });
+    // console.log(commentResponse.data)
+  } else {
+    console.log('PR title is in the correct format. Good job!')
+  }
+} catch (error) {
+  console.warn(error)
+}
