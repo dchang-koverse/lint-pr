62a660e794b41192d4d1d07786c5f9f0ec7d68e8
Dale Chang <dalechang@koverse.com>
2023-05-22 10:07:00 -0700

    Use request object

--- .github/scripts/lint-pr-title.js
+++ .github/scripts/lint-pr-title.js
@@ -3,15 +3,19 @@ import core from '@actions/core';
 
 const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
 const PR_NUMBER = core.getInput('PR_NUMBER');
+console.log('PR_NUMBER', PR_NUMBER)
 
 const octokit = new Octokit({
   auth: GITHUB_TOKEN,
 })
 
-const { data: pullRequest } = await octokit.rest.pulls.get({
+const { data: pullRequest } = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}', {
   owner: 'dchang-koverse',
   repo: 'lint-pr',
-  pull_number: PR_NUMBER
+  pull_number: PR_NUMBER,
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
 });
 
 console.log(pullRequest)
