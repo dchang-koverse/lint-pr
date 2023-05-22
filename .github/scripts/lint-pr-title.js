2acb825df83d8413319a9a98e5821d897fd8a210
Dale Chang <dalechang@koverse.com>
2023-05-22 10:42:56 -0700

    print context

--- .github/scripts/lint-pr-title.js
+++ .github/scripts/lint-pr-title.js
@@ -14,12 +14,13 @@ const root = await octokit.request("GET /")
 console.log(root.status)
 
 console.log('github', github)
+console.log('github.context.payload.pull_request.number', github.context.payload.pull_request.number)
 
 try {
   const response = await octokit.request("GET /repos/{owner}/{repo}/pulls/{pull_number}", {
     owner: 'dchang-koverse',
     repo: 'lint-pr',
-    pull_number: PR_NUMBER,
+    pull_number: github.context.payload.pull_request.number,
     headers: {
       'X-GitHub-Api-Version': '2022-11-28'
     }
