9bcb2a65d64c03dd9e3ed4579de5cb6a899bd20c
Dale Chang <dalechang@koverse.com>
2023-05-22 09:44:34 -0700

    get pr number

--- .github/scripts/lint-pr-title.js
+++ .github/scripts/lint-pr-title.js
@@ -2,10 +2,14 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+const PR_NUMBER = core.getInput('PR_NUMBER');
 
 const octokit = github.getOctokit(GITHUB_TOKEN);
 
-const { context = {} } = github;
-const { pull_request } = context.payload;
+const result = await octokit.rest.pulls.get({
+  owner: 'dchang-koverse',
+  repo: 'lint-pr',
+  pull_number: PR_NUMBER
+});
 
-console.log(pull_request.pull_request.title)
+console.log(result.data)
