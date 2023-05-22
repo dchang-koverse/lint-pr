20ed1b1ad2730077327cef596dda560f385b6d36
Dale Chang <dalechang@koverse.com>
2023-05-22 13:06:12 -0700

    Add error handling

--- .github/scripts/lint-pr-title.js
+++ .github/scripts/lint-pr-title.js
@@ -14,7 +14,7 @@ const root = await octokit.request("GET /")
 console.log(root.status)
 
 console.log('github', github)
-console.log('github.context.payload.pull_request.number', github.context.payload.pull_request.number)
+console.log('github.context.payload.pull_request', github.context.payload.pull_request)
 
 try {
   const response = await octokit.request("GET /repos/{owner}/{repo}/pulls/{pull_number}", {
@@ -26,7 +26,25 @@ try {
     }
   });
 
-  console.log(response)
+  const title = response.data.title;
+
+  console.log('title', title)
+
+  // Make sure title is in the format of "[CRUC-1234][UI] My title"
+  const regex = /\[CRUC-\d+\]\[.+\] .+/;
+  if (!regex.test(title)) {
+    // if the PR exists, but the title is not in the correct format, fail the check
+    core.setFailed('PR title is not in the correct format. Please use "[CRUC-1234][UI] My description" format.')
+    const commentResponse = await octokit.request("POST /repos/{owner}/{repo}/issues/{issue_number}/comments", {
+      owner: 'dchang-koverse',
+      repo: 'lint-pr',
+      issue_number: github.context.payload.pull_request.number,
+      body: 'PR title is not in the correct format. Please use "[CRUC-1234][UI] My description" format.'
+    });
+    console.log(commentResponse.data)
+  } else {
+    console.log('PR title is in the correct format. Good job!')
+  }
 } catch (error) {
   console.warn(error)
 }
