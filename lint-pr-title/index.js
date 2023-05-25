19f58fabb977a73043da025a0f0f674d99c7c977
Dale Chang <dalechang@koverse.com>
2023-05-25 10:44:01 -0700

    try to disable

--- lint-pr-title/index.js
+++ lint-pr-title/index.js
@@ -8,8 +8,6 @@ const octokit = new Octokit({
   auth: GITHUB_TOKEN,
 })
 
-console.log('github.context.payload.pull_request', github.context.payload.pull_request)
-
 try {
   const response = await octokit.request("GET /repos/{owner}/{repo}/pulls/{pull_number}", {
     owner: 'dchang-koverse',
@@ -21,21 +19,9 @@ try {
   });
 
   const title = response.data.title;
-
-  console.log('title', title)
-
-  // Make sure title is in the format of "[CRUC-1234][UI] My title"
   const regex = /\[CRUC-\d+\]\[.+\] .+/;
   if (!regex.test(title)) {
-    // if the PR exists, but the title is not in the correct format, fail the check
     core.setFailed('PR title is not in the correct format. Please use "[CRUC-1234][UI] My description" format.')
-    // const commentResponse = await octokit.request("POST /repos/{owner}/{repo}/issues/{issue_number}/comments", {
-    //   owner: 'dchang-koverse',
-    //   repo: 'lint-pr',
-    //   issue_number: github.context.payload.pull_request.number,
-    //   body: 'PR title is not in the correct format. Please use "[CRUC-1234][UI] My description" format.'
-    // });
-    // console.log(commentResponse.data)
   } else {
     console.log('PR title is in the correct format. Good job!')
   }
